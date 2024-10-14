3fc53399f337bef531619c7c3d6e292f758f2749
Saket <sr9101@srmist.edu.in>
2024-10-15 02:58:18 +0530

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+ class CelsiusToFahrenheit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter temperature in Celsius: ");
+        double c = sc.nextDouble();
+        double f = (c * 9/5) + 32;
+        System.out.println(c + "°C is equal to " + f+ "°F");
+
+    }
+}
